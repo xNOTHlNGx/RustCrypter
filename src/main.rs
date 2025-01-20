a4df5472a4832799891efaf8878997fbe03fe312
xNOTHlNGx <not_a_nothing@proton.me>
2025-01-19 22:42:44 -0500

    Enhanced arguments validation

--- src/main.rs
+++ src/main.rs
@@ -14,27 +14,33 @@ fn main() {
     let mut decrypt: bool = false; // Is decrypting
     let mut encrypt: bool = false; // Is encrypting
     for (index, arg) in args.iter().enumerate() {
-        if {arg == "-o" || arg == "--output"} && outfile.is_empty(){
-            outfile = args[index+1].clone();
+        if {arg == "-o" || arg == "--output"} && outfile.is_empty() {
+            outfile = if index+1 != args.len() && !args[index+1].starts_with("-") {args[index+1].clone()} else {println!("Error: output file isn't specified"); return};
         }
-        if arg == "-d" || arg == "--decrypt" {
+        else if {arg == "-d" || arg == "--decrypt"} && index != args.len()-1 {
             decrypt = true;
         }
-        if arg == "-e" || arg == "--encrypt" {
+        else if {arg == "-e" || arg == "--encrypt"} && index != args.len()-1 {
             encrypt = true;
         }
-        if arg == "-h" || arg == "--help" {
+        else if arg == "-h" || arg == "--help"  {
             println!("Help: {} -o <output> [-d] [-e] <input_file>", args[0]);
             return
         }
-    }
-    if args.len() == 3 || {args.len() == 5 && outfile.is_empty()} {
-        infile = args[args.len()-1].clone();
-        if encrypt == decrypt {
-            println!("Error: check arguments, must be either decrypt or encrypt, not both, not without it.\nFor more info type {} -h", args[0]);
-            return
+        else if index == args.len()-1 {
+            if args[index].starts_with("-") {
+                println!("Error: input file isn't specified");
+                return
+            }
+            infile = arg.clone();
         }
-        else if !infile.is_empty() {
+    }
+    if encrypt == decrypt {
+        println!("Error: check arguments, must be either decrypt or encrypt, not both, not without it.\nFor more info type {} -h", args[0]);
+        return
+    }
+    if args.len() == 3 || {args.len() == 5 && !infile.is_empty()} {
+        if !infile.is_empty() {
             if encrypt {
                 encryptfile(infile, outfile);
             }
